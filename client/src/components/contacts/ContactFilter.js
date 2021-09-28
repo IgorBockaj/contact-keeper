3a4a29a432dcf2faa74d85b3cbff4bfe08dbc4bc
Igor Bočkaj <igorbockaj268@gmail.com>
2021-09-28 07:42:07 +0200

    added context

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import ContactContext from "../../context/contact/contactContext";
+
+function ContactFilter() {
+  const contactContext = useContext(ContactContext);
+
+  return <div></div>;
+}
+
+export default ContactFilter;
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+
+function Home() {
+  return (
+    <div className="grid-2">
+      <ContactForm />
+      <div>
+        <Contacts />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
