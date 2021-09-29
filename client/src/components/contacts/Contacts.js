ec3f8eed8caf75c51e1dddb41c28fbbbe6842117
Igor Bočkaj <igorbockaj268@gmail.com>
2021-09-29 10:07:06 +0200

    Finished application

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactContext from "../../context/contact/contactContext";
 import ContactItem from "./ContactItem";
@@ -6,7 +6,12 @@ import ContactItem from "./ContactItem";
 function Contacts() {
   const contactContext = useContext(ContactContext);
 
-  const { contacts, filtered } = contactContext;
+  const { contacts, filtered, getContacts, loading } = contactContext;
+
+  useEffect(() => {
+    getContacts();
+    // eslint_disable-next-line
+  }, []);
 
   if (contacts.length === 0) {
     return <h4>Please add a contact</h4>;
@@ -17,11 +22,11 @@ function Contacts() {
       <TransitionGroup>
         {filtered !== null
           ? filtered.map((contact) => (
-              <ContactItem key={contact.id} contact={contact} />
+              <ContactItem key={contact._id} contact={contact} />
             ))
           : contacts.map((contact) => (
-              <CSSTransition key={contact.id} timeout={500} classNames="item">
-                <ContactItem key={contact.id} contact={contact} />
+              <CSSTransition key={contact._id} timeout={500} classNames="item">
+                <ContactItem contact={contact} />
               </CSSTransition>
             ))}
       </TransitionGroup>
